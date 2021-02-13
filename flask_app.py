9b20f33138712e08007150b18aaa39ebed3b9696
Niklas Hjort <niklas@astne.dk>
2021-02-13 02:53:07 +0100

    tilføjet hangman og snake

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, render_template
 from Utility import myReader, readWord, Client
 import nexmo
 
-
 descriptionDoc = 'static/assets/text/description.docx'
 educationDoc = 'static/assets/text/education.docx'
 jobDoc = 'static/assets/text/job.docx'
@@ -12,21 +11,23 @@ client = Client
 # sys.path.append('/Users/niklashjort/Desktop/Projects/Flask_Portfolio-master/venv/lib/python3.9/site-packages/flask_cors/')
 app = Flask(__name__)
 
-#print(sys.path)
+# print(sys.path)
 
 myTemplate = myReader
 readDocument = readWord
 
-
 app.config['JSON_ADD_STATUS'] = True
 app.config['JSON_DATETIME_FORMAT'] = '%d/%m/%Y %H:%M:%S'
-#dapd
+
+
+# dapd
 @app.route('/')
 def hello_world():
-  description = readDocument('static/assets/text/description.docx')
-  job = readDocument('static/assets/text/job.docx')
-  education = readDocument('static/assets/text/education.docx')
-  return render_template('index.html', description=description, job=job, education=education)
+    description = readDocument('static/assets/text/description.docx')
+    job = readDocument('static/assets/text/job.docx')
+    education = readDocument('static/assets/text/education.docx')
+    return render_template('index.html', description=description, job=job, education=education)
+
 
 @app.route('/Contact', methods=['GET', 'POST'])
 def aboutPage():
@@ -39,33 +40,33 @@ def aboutPage():
         client.sendSms(message)
     return render_template('/new/contact.html')
 
+
 @app.route('/project')
 def projectPage():
-  IMDB = readDocument('static/assets/text/IMDB.docx')
-  TD = readDocument('static/assets/text/TowerDefence.docx')
-  CodeLearn = readDocument('static/assets/text/codeLearningGame.docx')
-  Portfolio = readDocument('static/assets/text/portfolio.docx')
-  MineSweeper = readDocument('static/assets/text/mineSweeper.docx')
-  Maze = readDocument('static/assets/text/maze.docx')
-  Chicago = readDocument('static/assets/text/Chicago.docx')
-  Pokemon = readDocument('static/assets/text/pokemon.docx')
-  Database = readDocument('static/assets/text/database.docx')
-  return render_template('/new/proj.html',
-                         IMDB=IMDB,
-                         TD=TD,
-                         CodeLearn=CodeLearn,
-                         Portfolio=Portfolio,
-                         MineSweeper=MineSweeper,
-                         Maze=Maze,
-                         Chicago=Chicago,
-                         Pokemon=Pokemon,
-                         Database=Database)
-
+    IMDB = readDocument('static/assets/text/IMDB.docx')
+    TD = readDocument('static/assets/text/TowerDefence.docx')
+    CodeLearn = readDocument('static/assets/text/codeLearningGame.docx')
+    Portfolio = readDocument('static/assets/text/portfolio.docx')
+    MineSweeper = readDocument('static/assets/text/mineSweeper.docx')
+    Maze = readDocument('static/assets/text/maze.docx')
+    Chicago = readDocument('static/assets/text/Chicago.docx')
+    Pokemon = readDocument('static/assets/text/pokemon.docx')
+    Database = readDocument('static/assets/text/database.docx')
+    Snake = readDocument('static/assets/text/snake.docx')
+    Hangman = readDocument('static/assets/text/Hangman.docx')
+    return render_template('/new/proj.html',
+                           IMDB=IMDB,
+                           TD=TD,
+                           CodeLearn=CodeLearn,
+                           Portfolio=Portfolio,
+                           MineSweeper=MineSweeper,
+                           Maze=Maze,
+                           Chicago=Chicago,
+                           Pokemon=Pokemon,
+                           Database=Database,
+                           Snake=Snake,
+                           Hangman=Hangman)
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
-
-
-
-
+    app.run(debug=True)
